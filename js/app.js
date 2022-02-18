0ff3a36afadadefab8cd5e7e4834ed236615324d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-18 18:30:19 +0600

    get inocome input

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    //get income from input field
+    const income = document.getElementById("income-input");
+  const inc = parseFloat(income.value);
+
+});
