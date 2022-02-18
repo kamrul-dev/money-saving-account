4a5152458a4cc68cecca76258e3cf498c9f1ba14
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-18 19:25:41 +0600

    savings and reaming balance section done

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
             document.getElementById('food-input').value = '';
             document.getElementById('rent-input').value = '';
             document.getElementById('cloth-input').value = '';
-            
+            document.getElementById('save-input').value = '';
         }
     } else if (isNaN(inc) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothsCost)) {
         document.getElementById('total').innerText = "Please enter a number!";
@@ -38,6 +38,44 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
         document.getElementById('food-input').value = '';
         document.getElementById('rent-input').value = '';
         document.getElementById('cloth-input').value = '';
-        
+        document.getElementById('save-input').value = '';
     }
+});
+
+// save button even handler
+document.getElementById('save-btn').addEventListener('click', function () {
+
+    const saveInput = document.getElementById('save-input');
+    const savePercent = parseFloat(saveInput.value);
+    /*  checking percentage is 
+    not a string and 
+    not a negative number */
+    const income = document.getElementById('income-input');
+    const inc = parseFloat(income.value);
+    const balanced = document.getElementById("balance");
+    const bl = parseFloat(balanced.innerText);
+    const savingsAmout = (savePercent * inc) / 100;
+
+    /* 
+    savings should be 
+    less than current balance */
+    if (savePercent > 0) {
+        if (savingsAmout <= bl && savingsAmout > 0) {
+            document.getElementById('savingAmount').innerText = savingsAmout;
+            document.getElementById('remainingBalance').innerText = bl - savingsAmout;
+        } else {
+            document.getElementById('savingAmount').innerText = "Savings Amount can not be more than the current balance";
+            document.getElementById('savingAmount').style.color = "red";
+            document.getElementById('remainingBalance').innerText = "Savings Amount can not be more than the current balance";
+            document.getElementById('remainingBalance').style.color = "red";
+            document.getElementById('save-input').value = '';
+        }
+    } else if (isNaN(savePercent)) {
+        document.getElementById('savingAmount').innerText = "Please enter a number!";
+        document.getElementById('savingAmount').style.color = "red";
+        document.getElementById('remainingBalance').innerText = "Please enter a number";
+        document.getElementById('remainingBalance').style.color = "red";
+    }
+
+
 });
